0db286133366f37161d9f203f1462eb41be295e1
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-02-23 23:28:38 +0800

    Add site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,10 @@ require("dotenv").config({
 })
 
 module.exports = {
+  siteMetadata: {
+    title: `Liveinpure`,
+    siteUrl: `https://liveinpure.builtforfifty.com`,
+    description: `Liveinpure CRM App`,
+  },
   plugins: [`gatsby-plugin-sass`],
 }
