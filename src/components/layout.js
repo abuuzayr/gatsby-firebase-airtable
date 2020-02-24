879c13a2d2d4f881e44e24efa4a589b8c06a0bfb
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-02-25 00:58:13 +0800

    Update layout for company selection, breadcrumbs

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment, cloneElement } from 'react';
 import { Link } from 'gatsby';
 import * as ROUTES from '../constants/routes';
 
@@ -10,9 +10,17 @@ import '../styles/layout.scss'
 class Layout extends Component {
   state = {
     firebase: null,
+    companies: [],
+    company: ''
   };
 
-  componentDidMount() {
+  setCompany = (e) => {
+    this.setState({
+      company: e.currentTarget.value
+    })
+  }
+
+  async componentDidMount() {
     const app = import('firebase/app');
     const auth = import('firebase/auth');
     const database = import('firebase/database');
@@ -22,44 +30,80 @@ class Layout extends Component {
 
       this.setState({ firebase });
     });
+
+    try {
+      const companies = await fetch(`${process.env.GATSBY_STDLIB_URL}/getRawTableData?name=Companies`)
+      if (companies.status === 200) {
+        const body = await companies.json()
+        if (body.result === 'success') {
+          const companies = body.rows.map(row => row.fields['Name']).filter(Boolean)
+          this.setState({
+            companies,
+            company: this.state.company ? this.state.company : companies[0]
+          })
+        }
+      }
+    } catch (e) {
+      console.error(e)
+    }
   }
 
   render() {
     return (
       <FirebaseContext.Provider value={this.state.firebase}>
-        <AppWithAuthentication {...this.props} />
+        <AppWithAuthentication 
+          {...this.props} 
+          companies={this.state.companies}
+          company={this.state.company}
+          setCompany={this.setCompany}
+        />
       </FirebaseContext.Provider>
     );
   }
 }
 
-const AppWithAuthentication = withAuthentication(({ children, fullpage, page }) => (
-  <Fragment>
-    {!fullpage && <Navigation />}
-    {
-      !fullpage &&
-      <div className="container">
-        <div className="columns">
-          <div className="column">
-            <nav className="breadcrumb" aria-label="breadcrumbs">
-              <ul>
-                <li>
-                  <Link to={ROUTES.HOME}>
-                    CRM
-                  </Link>
-                </li>
-                <li className="is-active">
-                  <a href="#" aria-current="page">{page}                                        </a>
-                </li>
-              </ul>
-            </nav>
+const AppWithAuthentication = withAuthentication(props => {
+  const { children, fullpage, page, companies, company, setCompany } = props
+  return (
+    <Fragment>
+      {!fullpage && <Navigation />}
+      {
+        !fullpage &&
+        <div className="container">
+          <div className="columns">
+            <div className="column">
+              <nav className="breadcrumb" aria-label="breadcrumbs">
+                <ul>
+                  <li>
+                    <Link to={ROUTES.HOME}>
+                      CRM
+                    </Link>
+                  </li>
+                  <li className="is-active">
+                    <a href="#" aria-current="page">{page}</a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+            <div className="column">
+              <div className="is-pulled-right">
+                <small>Company: </small>
+                <select value={company} onChange={setCompany}>
+                  {
+                    companies && companies.map(company => (
+                      <option key={company} value={company}>{company}</option>
+                    ))
+                  }
+                </select>
+              </div>
+            </div>
           </div>
+          {cloneElement(children, { company: company ? company : companies[0] })}
         </div>
-        {children}
-      </div>
-    }
-    { fullpage && children }
-  </Fragment>
-));
+      }
+      { fullpage && children }
+    </Fragment>
+  )
+});
 
 export default Layout;
