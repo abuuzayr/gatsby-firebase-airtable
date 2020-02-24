d6197bc5aa55d80ab2404bc5c18c61f83303d83a
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-02-24 20:28:50 +0800

    Style logout button

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
+import { FiLogOut } from 'react-icons/fi'
+
 import { withFirebase } from '../Firebase';
 
 const SignOutButton = ({ firebase }) => (
   <button
     type="button"
     onClick={firebase ? firebase.doSignOut : () => {}}
+    className="button is-small is-danger is-outlined"
   >
+    <FiLogOut />
     Sign Out
   </button>
 );
