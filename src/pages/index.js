a8bd02b01ed037b7fb09a8ff2a40fb03a97e2b5f
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-02-25 11:11:05 +0800

    Redirect so logged in user does not signin again

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,8 @@
-import React, { Fragment } from 'react';
 import "../styles/main.scss"
-import Layout from '../components/layout';
-import { SignInPage } from './signin'
+import * as ROUTES from '../constants/routes';
+import { navigate } from 'gatsby'
 
-const LandingPage = () => (
-  <Fragment>
-    <h1>Landing</h1>
-    <p>
-      The Landing Page is open to everyone, even though the user isn't
-      signed in.
-    </p>
-  </Fragment>
-);
-
-export default () => (
-  <Layout fullpage>
-    <SignInPage />
-  </Layout>
-)
+export default () => {
+  navigate(ROUTES.HOME)
+  return null
+}
