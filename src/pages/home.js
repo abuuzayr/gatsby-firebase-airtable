a22e79745a3c0c3ed040be1fc702fef915aaa9e6
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-03-12 02:12:24 +0800

    Remove welcome banner

--- src/pages/home.js
+++ src/pages/home.js
@@ -276,18 +276,6 @@ const HomePageBase = (props) => {
           })
           return (
             <>
-              <section className="hero is-info welcome is-small">
-                <div className="hero-body">
-                  <div className="container">
-                    <h1 className="title">
-                      Hello, {authUser.username}.
-                      </h1>
-                    <h2 className="subtitle">
-                      I hope you are having a great day!
-                      </h2>
-                  </div>
-                </div>
-              </section>
               <section className="info-tiles">
                 <div className="tile is-ancestor has-text-centered">
                   <div className="tile is-parent">
