7b15cc8c247ff3c13f7bc820e01bb2be967aedeb
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-07-14 15:32:52 +0800

    Add fields for leads in modal

--- src/constants/fields.js
+++ src/constants/fields.js
@@ -158,6 +158,13 @@ export const fields = {
         'Payment Status'
     ],
     'Remarks': [
+    ],
+    'Leads': [
+        'Source',
+        'Name',
+        'Contact',
+        'No. of pax',
+        'Assign to',
     ]
 }
 
